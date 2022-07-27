2680cba4059e59e2da8e585f364dfcf2488a67b1
asmagnar <efdmartins@hotmail.com>
2022-07-27 18:02:24 +0100

    [WIP]updated checkconn
    add headers support
    add url property validation (this will be replicated with function to the other properties)

--- checkconn.py
+++ checkconn.py
@@ -1,40 +1,50 @@
+## Imports section
 import requests
 import os
 import sys
-
-## test using jproperties lib
-
-#import jproperties
-#from jproperties import Properties
-#
-#for filename in os.listdir(os.path.join(os.path.dirname(__file__), 'conf')):
-#    print (filename)
-#    if filename.endswith(".curl"):
-#        configs=Properties()
-#        with open(os.path.join(os.path.dirname(__file__), 'conf', filename), 'rb') as config_file:
-#            configs.load(config_file)
-#            print(configs.get("url"))
-
-
-## test using configparser ilb
 import configparser
+import json
 
-for filename in os.listdir(os.path.join(os.path.dirname(__file__), 'conf')):
-    configParser = configparser.RawConfigParser()
-    curlFile=os.path.join(os.path.dirname(__file__), 'conf', filename)
-    #configFilePath = r'curlfile'
-    configParser.read(curlFile)
-    print(url)
+## Functions that are goint to be used in the code:
+def create_request(url, method, headers, data, response):
+    r=requests.request(method=method,url=url,headers=headers,data=data)
+    if r.status_code==int(response):
+        return 0
+    else:
+        return r.status_code
+
+def validate_property():
+    #some code
+    print("stuff")
+
+filename="endpoints.curl"
+fileData = configparser.ConfigParser()
+curlFile=os.path.join(os.path.dirname(__file__), 'conf', filename) # File is in script root directory under /conf folder
+fileData.read(curlFile)
 
 
-#url = "https://www.example.com"
-#
-#request_response = requests.head(url)
-#status_code = request_response.status_code
-#website_is_up = status_code == 200
-#
-#requests.request(method=)
-#
-#print(website_is_up)
-## OUTPUT
-#True
+
+## Read each configuration in config file. 
+for section in fileData.sections():
+    if fileData.has_option(section,'url'): #validate if option exists
+        if fileData[section]['url'] =='': #validate if option has value
+            sys.exit("Error: Property url has no value! Please setup url in "+ curlFile +" config file") 
+        else:
+            url=fileData[section]['url']
+    else:
+        sys.exit("Error: No url defined! Please setup url in "+ curlFile +" config file") #if option does not have value program exits
+
+    method=fileData[section]['method']
+    headers=json.loads(fileData[section]['headers'])
+    data=fileData[section]['data']
+    response=fileData[section]['response']
+    result=create_request(url, method, headers, data, response)
+    print(result)
+
+#endpoints.curl file:
+#[IPG TEST EXAMPLE]
+#url=
+#method=
+#headers={"x-api-key": "PMAK-62e14ee9ce05de328ea1af2a-adba641dba080a40730c6e12e24f3ef0aa", "testheader": "testvalue"}
+#data=
+#response=500
